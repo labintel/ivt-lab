ff3497b17585e45bd2283e85f7d92006ef526061
labintel <zsemlebuci@gmail.com>
2023-04-28 21:24:36 +0200

    Fixed error

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,7 +80,18 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
 
-        break;
+        boolean primaryDone = false;
+        boolean secondaryDone = false;
+
+        if (! primaryTorpedoStore.isEmpty()) {
+          primaryDone = primaryTorpedoStore.fire(1);
+        }
+
+        if (! secondaryTorpedoStore.isEmpty()) {
+          secondaryDone = secondaryTorpedoStore.fire(1);
+        }
+
+        firingSuccess = primaryDone || secondaryDone;
     }
 
     return firingSuccess;
